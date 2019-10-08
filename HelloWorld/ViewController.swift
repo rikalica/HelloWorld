98c7fd967cbf0ad2284f48c9dd09bc1a2c951f1c
rika.hataji <hatajimui@yahoo.co.jp>
2019-10-08 18:26:55 +0900

    HelloWorldのラベルを追加した

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 幡地美佳 on 2019/10/08.
+//  Copyright © 2019 rika.hataji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
